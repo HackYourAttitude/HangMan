8b056c491c68d2782f3c3dc84efa4ada51593a4a
Marcin Zyla <marcin.zyla13@gmail.com>
2021-12-31 18:00:13 +0100

    Read from File module, Random Word,difficulty level implementation

---
+++ HangMan/HangMan/OpenFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace HangMan
+{
+    internal class OpenFile
+    {
+        public static string ReadFile(string filePath)
+        {
+            byte[] b = new byte[1024];
+            UTF8Encoding temp = new UTF8Encoding(true);
+            using (FileStream FS = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+               FS.Read(b,0,b.Length);
+            }
+            return temp.GetString(b);
+        }
+
+    }
+}
---
+++ HangMan/HangMan/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using HangMan;
+
+namespace HangMan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = OpenFile.ReadFile("C:\\Users\\48602\\Desktop\\HangMan\\CountriesCapitals.csv");
+            string word = Program.GiveSingleWord(words);
+            string level = Program.ChooseDifficultyLevel();
+
+        }
+
+        public static string GiveSingleWord(string words)
+        {
+            Random random = new Random();
+            var ListOfWords = words.Trim().Split('\n', ' ');
+            return ListOfWords[random.Next(ListOfWords.Length)];
+        }
+
+        public static string ChooseDifficultyLevel()
+        {
+            Console.WriteLine("Choose Game Level :");
+            Console.WriteLine("");
+            Console.WriteLine("Press '1' for Easy ==> 5 lifes");
+            Console.WriteLine("Press '2' for Midium ==> 4 lifes");
+            Console.WriteLine("Press '3' for Hard ==> 3 lifes");
+            while (true)
+            {
+                string level = Console.ReadLine();
+                string[] options = { "1", "2", "3" };
+                if (level.Length == 1 && options.Contains(level))
+                    return level;
+                Console.WriteLine("Sorry wrong input  !!! Try again");
+            }
+
+
+        }
+
+    }
+
+}
